f6744d5bc36310ed62c16c42e7797eb624c9801d
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-09-18 17:36:13 +0200

    using find() to select elements by classes

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://webscraper.io/test-sites/e-commerce/allinone/computers'
+# url = 'https://webscraper.io/test-sites/e-commerce/allinone/computers'
+#
+# page = requests.get(url)
+#
+# soup = BeautifulSoup(page.text, 'lxml')
+# print(soup)
+# print('--------------------------')
+# print(soup.header)
+# print('--------------------------')
+# tag = soup.header.p.string
+# print(tag)
+# print('--------------------------')
+# tag = soup.header.a.attrs
+# print(tag)
 
-page = requests.get(url)
+new_url = 'https://webscraper.io/test-sites/e-commerce/allinone/phones/touch'
+new_page = requests.get(new_url)
+new_soup = BeautifulSoup(new_page.text, 'lxml')
+# header_test = new_soup.find('header')
+# print(header_test)
+print('--------------------------')
+new_div = new_soup.find('div', {'class': 'container test-site'})
+print(new_div)
+print('--------------------------')
 
-soup = BeautifulSoup(page.text, 'lxml')
-print(soup)
-print('--------------------------')
-print(soup.header)
-print('--------------------------')
-tag = soup.header.p.string
-print(tag)
-print('--------------------------')
-tag = soup.header.a.attrs
-print(tag)
+new_element = new_soup.find('h4', {'class': 'pull-right price'})
+print(new_element)
